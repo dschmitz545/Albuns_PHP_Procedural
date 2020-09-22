1df0e927cac21bfc78f29bde3ce04f9d68187f26
Diego Schmitz <diego.sctar@gmail.com>
2020-09-22 20:49:39 -0300

    Definido layout do bootstrap, criado array de albuns e definido algumas variaveis do php

---
+++ includes/dados.php
@@ -0,0 +1,24 @@
+<?php
+ $titulo = "Meu Site PHP";
+ $subtitulo = "Primeira tela PHP+HTML";
+
+ $albuns = [
+     [
+         'titulo' => 'Shank',
+         'descricao' => 'Album de 2000',
+         'capa' => '',
+         'duracao' => '9 mins'
+     ],
+     [
+         'titulo' => 'Titãs',
+         'descricao' => 'Álbum de 1999',
+         'capa' => '',
+         'duracao' => '11 mins'
+     ],
+     [
+        'titulo' => 'Zeca pagodinho',
+        'descricao' => 'Deixa a vida me levar',
+        'capa' => '',
+        'duracao' => '15 mins'
+    ],
+ ];
